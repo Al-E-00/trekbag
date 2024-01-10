02a0900411dc1450ce0b4f187fe929464943a53f
Alessio <alessio.mazzella@siren.io>
2024-01-10 20:10:00 +0100

    Add error handling for missing context in useItemsContext

--- src/lib/hooks.js
+++ src/lib/hooks.js
@@ -3,5 +3,12 @@ import { ItemsContext } from "../components/context/ItemsContextProvider";
 
 export function useItemsContext() {
   const context = useContext(ItemsContext);
+
+  if (!context) {
+    throw new Error(
+      "useItemsContext must be used within a ItemsContextProvider"
+    );
+  }
+
   return context;
 }
